c89b6b16f006b2a1111c81a11d3942671d1a0c8d
ldclakmal <ldclakmal@gmail.com>
2017-11-07 16:44:03 +0530

    Remove response classes

--- src/main/java/org/uom/cse/cs4262/api/message/response/JoinResponse.java
+++ src/main/java/org/uom/cse/cs4262/api/message/response/JoinResponse.java
@@ -1,44 +1,44 @@
-package org.uom.cse.cs4262.api.message.response;
-
-import org.uom.cse.cs4262.api.Credential;
-import org.uom.cse.cs4262.api.message.Message;
-
-/**
- * @author Chandu Herath
- * @date 23/10/2017
- * @since 1.0
- */
-
-public class JoinResponse extends Message {
-
-    private int value;
-
-    private Credential senderCredential;
-
-    public JoinResponse(int value, Credential senderCredential) {
-        this.value = value;
-        this.senderCredential = senderCredential;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public Credential getSenderCredential() {
-        return senderCredential;
-    }
-
-    public void setSenderCredential(Credential senderCredential) {
-        this.senderCredential = senderCredential;
-    }
-
-    @Override
-    public String getMessageAsString(String message) {
-        message += " " + this.getValue();
-        return super.getMessageAsString(message);
-    }
-}
+//package org.uom.cse.cs4262.api.message.response;
+//
+//import org.uom.cse.cs4262.api.Credential;
+//import org.uom.cse.cs4262.api.message.Message;
+//
+///**
+// * @author Chandu Herath
+// * @date 23/10/2017
+// * @since 1.0
+// */
+//
+//public class JoinResponse extends Message {
+//
+//    private int value;
+//
+//    private Credential senderCredential;
+//
+//    public JoinResponse(int value, Credential senderCredential) {
+//        this.value = value;
+//        this.senderCredential = senderCredential;
+//    }
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(int value) {
+//        this.value = value;
+//    }
+//
+//    public Credential getSenderCredential() {
+//        return senderCredential;
+//    }
+//
+//    public void setSenderCredential(Credential senderCredential) {
+//        this.senderCredential = senderCredential;
+//    }
+//
+//    @Override
+//    public String getMessageAsString(String message) {
+//        message += " " + this.getValue();
+//        return super.getMessageAsString(message);
+//    }
+//}
--- src/main/java/org/uom/cse/cs4262/api/message/response/LeaveResponse.java
+++ src/main/java/org/uom/cse/cs4262/api/message/response/LeaveResponse.java
@@ -1,32 +1,32 @@
-package org.uom.cse.cs4262.api.message.response;
-
-import org.uom.cse.cs4262.api.message.Message;
-
-/**
- * @author Chandu Herath
- * @date 23/10/2017
- * @since 1.0
- */
-
-public class LeaveResponse extends Message {
-
-    private int value;
-
-    public LeaveResponse(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    @Override
-    public String getMessageAsString(String message) {
-        message += " " + this.getValue();
-        return super.getMessageAsString(message);
-    }
-}
+//package org.uom.cse.cs4262.api.message.response;
+//
+//import org.uom.cse.cs4262.api.message.Message;
+//
+///**
+// * @author Chandu Herath
+// * @date 23/10/2017
+// * @since 1.0
+// */
+//
+//public class LeaveResponse extends Message {
+//
+//    private int value;
+//
+//    public LeaveResponse(int value) {
+//        this.value = value;
+//    }
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(int value) {
+//        this.value = value;
+//    }
+//
+//    @Override
+//    public String getMessageAsString(String message) {
+//        message += " " + this.getValue();
+//        return super.getMessageAsString(message);
+//    }
+//}
